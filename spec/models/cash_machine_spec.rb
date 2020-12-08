1b8fa60e1d6e8d24218ed09f388be4629dc4c1ac
AnotherMetal99 <62662423+AnotherMetal99@users.noreply.github.com>
2020-12-08 17:37:31 +0300

    rubocop

---
+++ spec/models/cash_machine_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe CashMachine, type: :model do
+  before(:all) do
+    @cash_machine = CashMachine.create(id: 1)
+    @cash_machine.reload # for sync with db
+    @i = -2
+    @u = User.new(email: 'user@gmail.com', password: '123456')
+    expect(@u.valid?).to eq(true)
+  end
+  after(:all) do
+    @cash_machine.destroy
+    @u.destroy
+  end
+  context 'add/update cash' do
+    it 'Add cash' do
+      while @i < 2
+        # it{expect('').to be_instance_of(String)}
+        expect(CashMachine.add_cash(@i)).to be_truthy
+        @i += 1
+      end
+    end
+
+    it 'Available cash' do
+      while @i < 10
+        expect(CashMachine.cash_available?(@i, @u)).to be_truthy
+        @i += 1
+      end
+      @i = 999_999 # don`t anogh money. Expect false
+      expect(CashMachine.cash_available?(@i, @u)).to be_falsey
+    end
+
+    # it "add" do
+    #   expect(CashMachine.add_cash(12)).to be_truthy
+    # end
+
+    #
+    # it{expect(12).to be_a_new(Integer)}
+    # it{expect(CashMachine.add_cash('')).to be_falsey}
+    # it{expect(CashMachine.add_cash('1')).to be_falsey}
+    # it{expect(CashMachine.add_cash('12')).to be_falsey}
+  end
+end
